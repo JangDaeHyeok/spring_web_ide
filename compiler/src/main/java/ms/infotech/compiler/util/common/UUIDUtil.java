bfd03bb48bd47104394da7fd9eaedc955c20dadf
JandDaeHyeok <jangdaehyeok@gmail.com>
2022-05-27 16:50:01 +0900

    주석 수정

--- compiler/src/main/java/ms/infotech/compiler/util/common/UUIDUtil.java
+++ compiler/src/main/java/ms/infotech/compiler/util/common/UUIDUtil.java
@@ -3,21 +3,8 @@ package ms.infotech.compiler.util.common;
 import java.util.UUID;
 
 public class UUIDUtil {
-	/**
-	 * @Developer 이강민
-	 * @Description UUID 생성시 "-" 제거
-	 */
 	public static String createUUID() {
 		String uuid = UUID.randomUUID().toString().replace("-","");
-		return uuid;
-	}
-	
-	/**
-	 * @Developer 이강민
-	 * @Description UUID 생성시 "-" 제거 및 16자리 숫자로 Cut
-	 */
-	public static String createUUID16() {
-		String uuid = UUID.randomUUID().toString().replace("-","").substring(0, 16).toUpperCase();
 		return uuid;
 	}
 }
--- compiler/src/main/java/ms/infotech/compiler/util/server/ServerOffUtil.java
+++ compiler/src/main/java/ms/infotech/compiler/util/server/ServerOffUtil.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class ServerOffUtil {
-	@Scheduled(cron = "0 0 */3 * * *")
+	// @Scheduled(cron = "0 0 */3 * * *")
 	public void stopTomcatTest() throws Exception {
 		log.info("[ServerOffUtil] 서버 종료");
 		System.exit(1);
